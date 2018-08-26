763787e2a7a880da78e7fbe321db3afe837e249f
edward <edward.huang@foxmail.com>
2018-08-26 16:30:57 +0800

    3.1-小程序生面周期代码

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 //app.js
 App({
+  onLaunch:function(){
+
+    console.log("小程序加载");
+
+    var logs = wx.getStorageSync('logs') || [];
+
+    logs.unshift(Date.now());
+
+    wx.setStorageSync('logs', logs);
+
+  },
+  onShow:function(){
+    console.log("小程序显示");
+  },
+  onHide:function(){
+    console.log("小程序隐藏");
+  },
+  onError:function(msg){
+    console.log(msg)
+  }
+  /*
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
@@ -36,4 +57,5 @@ App({
   globalData: {
     userInfo: null
   }
+  */
 })
